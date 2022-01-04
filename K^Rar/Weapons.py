1faaaf8341e813a328545e2218192166185c99d9
Playimmersive <79789175+Playimmersive@users.noreply.github.com>
2022-01-04 08:10:00 +0100

    Update
    Weapons.py

--- K^Rar/Weapons.py
+++ K^Rar/Weapons.py
@@ -1,11 +1,15 @@
 
 ###                   Waffenliste                ###
 
-dagger = (3, "dagger")
-log = (8, "log")
-sword = (15, "sword")
-sharped_dagger = (5, "sharped dagger")
+
+#weapon                               (attackpoints,    weapon)
+
+dagger                =               (3,             "dagger")
+log                   =               (8,                "log")
+sword                 =               (15,             "sword")
+sharped_dagger        =               (5,     "sharped dagger")
 
 ###         Abfrage ob Waffe gefunden wurde      ###
+
 
 sharped_dagger_found = False
