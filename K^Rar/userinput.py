3b6e0e97f2e46948110a4e85b4716a855d89ee5b
BlackLNX <nkliesow@hotmail.de>
2022-01-04 15:15:58 +0100

    Update userinput.py

--- K^Rar/userinput.py
+++ K^Rar/userinput.py
@@ -7,8 +7,8 @@ option_list = list()
 
 def user_option(name, text, callback):
                                                   #OPTIONAL, Vor erstem Eintrag Ankündigung der Eingabe anzeigen
-  if len(option_list) == 0: print("Choose an action what to do:")
-  print("Type '", name, "' to ", text)
+  if len(option_list) == 0: print("Choose an action what to do:\n")
+  print("Type '" + name + "' to " + text)
   option_list.append([name, callback])            #Der Optionen-Liste die Option hinzufügen
  
 
@@ -16,16 +16,16 @@ def user_option(name, text, callback):
 def user_prompt():
   i = 0
   print("")                                        #Eine Zeile Abstand
-  choice =  input(">").trim().lower()              #Zeileneingabe Whitespace entfernen und Kleinschreibung erzwingen
+  choice =  input(">").strip().lower()              #Zeileneingabe Whitespace entfernen und Kleinschreibung erzwingen
   for option in option_list:
-    i++                                            #Entweder den Befehl gefunden oder übereinstimmung der Nummer der Option
-    if (option[0] == choice) OR (i == choice):
+    i+=1                                           #Entweder den Befehl gefunden oder übereinstimmung der Nummer der Option
+    if (option[0] == choice) or (i == choice):
       option_list.clear()                          #Optionen-Liste löschen für nächste Abfrage
       return option[1]()                           #Callback-Funktion aufrufen
                                                    #Wenn keine Option gefunden wurde
   sys.stdout.write("\033[F")                       #OPTIONAL, nach oben springen
   print("Sorry I did not understand '", choice, "', please pick an action from above")
-  return user_prompt()                             #Erneut abfragen
+  return user_prompt()                             #Erneut abfragen 
 
 
 
