5b83b3f347e16013eb571c5c35c1965858de4aae
Playimmersive <79789175+Playimmersive@users.noreply.github.com>
2022-01-26 21:15:47 +0100

    Add files via upload

--- K^Rar/level-felzur.py
+++ K^Rar/level-felzur.py
@@ -18,9 +18,31 @@ Orte: felzur_city
 
 
 ###                               Felzur City Gate                            ###
+def felzur_city_gate():
+  player1.location = felzur_city_gate
+  screen_clear()
+  slow_print("\nThere is the city gate in front of "+ player1.name + ".\n")
+  player_input = (input("\n\nType 'leave' to pass through the gate and leave the city.\nType 'alley' to go back to the city alley.\n\n>"))
+  if "leave" in player_input:
+    world_map_felzur_city()
+  elif "alley" in player_input:
+    felzur_city_alley()
+  else:slow_print("Can't handle your input!")
+  this_location() 
 
 
-
+###                               felzur_city_gate_entrance                            ###
+def felzur_city_gate_entrance():
+  player1.location = felzur_city_gate_entrance
+  screen_clear()
+  slow_print("\nThere is the city gate in front of "+ player1.name + ".\n")
+  player_input = (input("\n\nType 'enter' to pass through the gate and enter the city alley.\nType 'map' to go back to the regional map.\n\n>"))
+  if "enter" in player_input:
+    felzur_city_alley()
+  elif "map" in player_input:
+    world_map_felzur_city()
+  else:slow_print("Can't handle your input!")
+  this_location() 
 
 ###                               Felzur City Alley                           ###
 
@@ -30,15 +52,16 @@ def felzur_city_alley():
   screen_clear()
   slow_print("\n" + player1.name +" enters the Felzur city alley.\nThe alley leads from the citygate to the plaza of Felzur.\nThere is nobody at the allay, it's cold and windy and the whole ground lies under a thin skin of ice.\n" + player1.name  + " has a room here aside the bar.\n")
 
-  player_input = (input("\nType 'room' to go to your little home.\nType 'bar' to enter the city bar.\nType 'plaza' leave the allay and to go to the city plaza.\nType 'rest' to rest a while\n"))
+  player_input = (input("\nType 'room' to go to your little home.\nType 'gate' to go to the city gate.\nType 'bar' to enter the city bar.\nType 'plaza' leave the allay and to go to the city plaza.\nType 'rest' to rest a while\n\n"))
   if "room" in player_input:
     felzur_city_player_home()
+  elif "gate" in player_input:
+    felzur_city_gate()
   elif "plaza" in player_input:
     felzur_city_plaza()
   elif "bar" in player_input:
     felzur_city_bar()
   elif "rest" in player_input:
-    print(player1.name + " finds a place to rest.\n!")
     player1.rest_unsafe()
   elif "menu" in player_input:
     start_menu()
@@ -66,7 +89,7 @@ def felzur_city_player_home():
 def felzur_city_bar():
   player1.location = felzur_city_bar
   screen_clear()
-  slow_print("\n" + player1.name + " can see some dark figures around a dirty table.\nThe barkeeper looks not realy inviting like the rest of this bar.\n")
+  slow_print("\n" + player1.name + " can see some dark figures around a dirty table.\nThe barkeeper looks not realy inviting like the rest of this bar.\n\n")
   player_input = (input("\nType 'talk' to start a conversation.\nType 'rest' to take place.\nType 'meal' to buy a meal. (9 Gold)\nType 'drink' to buy a drink. (6 gold) \nType 'leave' to leave the bar.\nType 'quit' to go back to the start menu.\n\n>"))
   if "meal" in player_input:
     if player1.gold > 9:
@@ -116,7 +139,7 @@ def felzur_city_bar():
 def felzur_city_plaza():
   player1.location = felzur_city_plaza
   screen_clear()
-  slow_print("\n" + player1.name + " enters the Felzur city plaza.\nThere are some buildings around a windy place.\nIn the middle of the place is a huge coluseum.\nIn front of it is a little market with some merchants.\n")
+  slow_print("\n" + player1.name + " enters the Felzur city plaza.\nThere are some buildings around a windy place.\nIn the middle of the place is a huge coluseum.\nIn front of it is a little market with some merchants.\n\n")
 
   if stray_dog_01.alive == True:
     slow_print("There is a dog in front of a little shack.\n\nType 'pet' to pet him.")
@@ -159,31 +182,82 @@ def felzur_city_plaza():
 def felzur_city_market():
   player1.location = felzur_city_market
   screen_clear()
-  slow_print("\n" + player1.name + " goes to the market, there are some merchants and soldiers around the market.\n")
-  player_input = (input("\nType 'sell fur' to sell fur. (15 gold)\nType 'buy sword' to buy a sword. (150 gold) \nType 'leave' to leave the market.\nType 'rest' to take a little break.\n\n>"))
+  slow_print("\n" + player1.name + " goes to the market, there are some merchants and soldiers around the market.\n(" + player1.name + " has " + str(player1.gold) + " gold.)\n")
+  player_input = (input("\nType 'sell fur' to sell fur. (" + str(fur_price) + " gold)\nType 'sell herbs' to sell herbs. (" + str(herbs_price) + " gold)\nType 'sell water' to sell water. (" + str(water_price) + " gold)\nType 'sell meal' to sell meals. (" + str(meals_price) + " gold)\n\nType 'buy log' to buy a log. (" + str(log[2]) + " gold)\nType 'buy sharped dagger' to buy a sharped dagger. (" + str(sharped_dagger[2]) + " gold) \nType 'buy sword' to buy a sword. (" + str(sword[2]) + " gold) \n\nType 'leave' to leave the market.\nType 'rest' to take a little break.\n\n>"))
   if "sell fur" in player_input:
     if player1.fur > 0:
-      slow_print(player1.name + " goes into a little tent and sells a fur for 15 gold, that was a good deal!\n")
-      player1.gold = player1.gold + 15
+      slow_print(player1.name + " goes into a little tent and sells a fur for " + str(fur_price) + " gold, that was a good deal!\n")
+      player1.gold = player1.gold + fur_price
       player1.fur = player1.fur - 1
       this_location() 
     else: player1.fur < 1
     slow_print(player1.name + " has no fur!\n")
-    this_location() 
+    this_location()
 
-  elif "buy sword" in player_input:
-    if player1.gold > 149:
-      slow_print(player1.name + " walkes to the blacksmith and buys a sword for 150 gold.\nIt has a very sharp blade with some beautiful decorations in it!\n")
-      player1.gold = player1.gold - 150
-      player1.weapon = sword
+    
+  elif "sell herbs" in player_input:
+    if player1.medics > 0:
+      slow_print(player1.name + " goes into a little tent and sells a herbs for " + str(herbs_price) + " gold, that was a good deal!\n")
+      player1.gold = player1.gold + herbs_price
+      player1.medics = player1.medics - 1
+      this_location() 
+    else: player1.medics < 1
+    slow_print(player1.name + " has no herbs!\n")
+    this_location()
+
+  elif "sell water" in player_input:
+    if player1.water > 0:
+      slow_print(player1.name + " goes into a little tent and sells a water for " + str(water_price) + " gold, that was a good deal!\n")
+      player1.gold = player1.gold + water_price
+      player1.water = player1.water - 1
+      this_location() 
+    else: player1.water < 1
+    slow_print(player1.name + " has no water!\n")
+    this_location()
+
+  elif "sell meal" in player_input:
+    if player1.meals > 0:
+      slow_print(player1.name + " goes into a little tent and sells a meal for " + str(meals_price) + " gold, that was a good deal!\n")
+      player1.gold = player1.gold + meals_price
+      player1.meals = player1.meals - 1
+      this_location() 
+    else: player1.meals < 1
+    slow_print(player1.name + " has no meal!\n")
+    this_location()
+
+  elif "buy sharped dagger" in player_input:
+    if player1.gold >= sharped_dagger[2]:
+      slow_print(player1.name + " walkes to the blacksmith and buys a sharped dagger for " + str(sharped_dagger[2]) + " gold.\nIt has some scratches at the blade!\n")
+      player1.gold = player1.gold - sharped_dagger[2]
+      player1.weapon = sharped_dagger
       this_location() 
     else: player1.gold < 150
     slow_print(player1.name + " has not enough gold!\n")
     this_location() 
 
+  elif "buy log" in player_input:
+    if player1.gold >= log[2]:
+      slow_print(player1.name + " walkes into a little tent and buys a log for " + str(log[2]) + " gold.\n\n")
+      player1.gold = player1.gold +log[2]
+      player1.weapon = log
+      this_location() 
+    else: player1.gold < 70
+    slow_print(player1.name + " has not enough gold!\n")
+    this_location() 
+    
+  elif "buy sword" in player_input:
+    if player1.gold >= sword[2]:
+      slow_print(player1.name + " walkes to the blacksmith and buys a sword for " + str(sword[2]) + " gold.\nIt has a very sharp blade with some beautiful decorations in it!\n")
+      player1.gold = player1.gold - sword[2]
+      player1.weapon = sword
+      this_location() 
+    else: player1.gold < sword[2]
+    slow_print(player1.name + " has not enough gold!\n")
+    this_location() 
+
   elif "rest" in player_input:
-    print(player1.name + "\n")
     player1.rest_unsafe()
+    
   elif "leave" in player_input:
     felzur_city_plaza()
   else:print("Can't handle your input!")
@@ -235,12 +309,14 @@ def felzur_city_plaza_shack_01():
         player_input = (input("\nPress 'equip' to equip the sharped dagger.\nPress 'sell' to sell it at the city place.\n>"))
         if "equip" in player_input:
           slow_print(player1.name + " equipes the sharped dagger.\n")
+          input("\n>")
           sharped_dagger_found = True
           player1.weapon = sharped_dagger
         elif "sell" in player_input:
           slow_print(player1.name + " sells the sharped dagger at the city plaza for 22 gold.\n")
           player1.gold = player1.gold + 22
           sharped_dagger_found = True
+          input("\n>")
           felzur_city_plaza()
           
         else:slow_print("Can´t handle your input!")
@@ -289,7 +365,8 @@ def felzur_city_plaza_stonebuilding_01():
   elif "talk" in player_input:
       dialog_bandit2()
   elif "plaza" in player_input:
-      slow_print(player1.name + " leaves the stonebuilding.\n\n")
+      slow_print(player1.name + " leaves the stonebuilding.")
+      input("\n>")
       felzur_city_plaza()
 
   else:slow_print("Can't handle your input!\n>")
