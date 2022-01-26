5dc715e540adde00e0ea3c5fc587cc01d33e7f94
Playimmersive <79789175+Playimmersive@users.noreply.github.com>
2022-01-26 21:39:02 +0100

    Update

--- K^Rar/weapons.py
+++ K^Rar/weapons.py
@@ -4,15 +4,13 @@
 
 #weapons human        =               (attackpoints,    weapon,   prize)
 
-dagger                =               (4,             "dagger",     10)
-log                   =               (8,                "log",     80)    
+dagger                =               (4,             "dagger",     20)
+log                   =               (7,                "log",     75)    
 sword                 =               (13,             "sword",     250)
-sharped_dagger        =               (5,     "sharped dagger",     22)
-
+sharped_dagger        =               (5,     "sharped dagger",     40)
+guard_sword           =               (15,     "guard sword",      300)
 
 #weapons animal       =             (attackpoints,    weapon,   prize)
-
-
 fangs                 =               (2,             "fangs",  13)
 claw                  =               (3,             "claw",    5)
 horn                  =               (10,            "horn",   65)
